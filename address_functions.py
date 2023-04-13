2dfc9e6aaf6c29c1cdf8f9b3b26056a5f63739af
phantrunglam <phantrunglam@gmail.com>
2023-04-13 11:22:31 +0700

    aws sam app version
    TODO: fix this deployment error:
    Error: Failed to create changeset for the stack: vietfi-address-code, An error occurred (ValidationError) when calling the CreateChangeSet operation: Invalid template property or properties [AddressFunctionIamRole, AddressFunction]

---
+++ address_functions.py
@@ -0,0 +1,88 @@
+import sqlite3
+import json
+from email.header import UTF8
+
+from flask import abort, jsonify, Response
+from flask_restful import Resource
+
+
+class CountriesResource(Resource):
+    def get(self):
+        try:
+            conn = sqlite3.connect('db/address_db.sqlite3')
+            c = conn.cursor()
+            c.execute("SELECT countryid, country_name, iso3, iso2, phonecode FROM sys_country")
+            countries = c.fetchall()
+            conn.close()
+            return jsonify(countries)
+        except Exception as e:
+            abort(500, str(e))
+
+class CountryCDResource(Resource):
+    def get(self, country_code):
+        try:
+            conn = sqlite3.connect('db/address_db.sqlite3')
+            c = conn.cursor()
+            c.execute("SELECT countryid, country_name, iso3, iso2, phonecode FROM sys_country \
+                        WHERE iso3 = ?", (country_code,))
+            country = c.fetchall()
+            conn.close()
+            #country = "Việt nam"
+            str = jsonify(country)
+            #print("str =" + str)
+
+            #response = Response(jsonify(country), content_type="application/json; charset=utf-8")
+            #print("response =" + response)
+            return str
+        except Exception as e:
+            abort(500, str(e))
+
+class DivisionsResource(Resource):
+    def get(self, countryid):
+        try:
+            conn = sqlite3.connect('db/address_db.sqlite3')
+            c = conn.cursor()
+            c.execute("SELECT  divisionid, division_cd, division_name, gso_code, los_code \
+	                    FROM  sys_division \
+	                    WHERE countryid = ?", (countryid,))
+            divisions = c.fetchall()
+            conn.close()
+            response = jsonify(divisions)
+            response.headers["Content-Type"] = "application/json"
+            # print("str =" + str)
+            # response = Response(str, content_type="application/json; charset=utf-8")
+            # print("response =" + response)
+            return response
+        except Exception as e:
+            abort(500, str(e))
+
+class DivisionIDResource(Resource):
+    def get(self, countryid,divisionid):
+        try:
+            conn = sqlite3.connect('db/address_db.sqlite3')
+            c = conn.cursor()
+            c.execute("SELECT  divisionid, division_cd, division_name, gso_code, los_code \
+	                    FROM  sys_division \
+	                    WHERE countryid = ? AND divisionid = ?", (countryid,divisionid))
+            division = c.fetchall()
+            conn.close()
+            return jsonify(division)
+        except Exception as e:
+            abort(500, str(e))
+class SubdivisionsResource(Resource):
+    def get(self, countryid, divisionid):
+        try:
+            conn = sqlite3.connect('db/address_db.sqlite3')
+            c = conn.cursor()
+            c.execute("SELECT subdivid, subdiv_cd, l2subdiv_cd, subdiv_name, los_code_sub,b.divisionid, b.countryid \
+                        from sys_division_sub a, sys_division b \
+                        WHERE a.divisionid = b.divisionid \
+                        AND b.countryid = ? AND a.divisionid = ? \
+                        ORDER by a.subdivid", (countryid, divisionid))
+
+            subdivisions = c.fetchall()
+            conn.close()
+            return jsonify(subdivisions)
+        except Exception as e:
+            abort(500, str(e))
+
