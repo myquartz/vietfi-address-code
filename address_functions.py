dcac7d07384deafea1aeb3facad0a7d5466494b1
phantrunglam <phantrunglam@gmail.com>
2023-04-14 21:07:38 +0700

    focus on db file and tmp
    Thạch Anh xem lại nhé

--- address_functions.py
+++ address_functions.py
@@ -5,12 +5,30 @@ from email.header import UTF8
 from flask import abort, jsonify, Response
 from flask_restful import Resource
 
+DB_DIR = '/tmp/'
 
 class CountriesResource(Resource):
     def get(self):
         try:
-            conn = sqlite3.connect('db/address_db.sqlite3')
+            conn = sqlite3.connect(DB_DIR + "address_db.sqlite3")
             c = conn.cursor()
+            try:
+                dbres = dbcur.execute("SELECT name FROM sqlite_master WHERE name='access_log'")
+                if dbres.fetchone() is None:
+                    dbcreate = dbcur.execute("""
+                    CREATE TABLE IF NOT EXISTS access_log (
+                        REQ_ID VARCHAR(64) PRIMARY KEY,
+                        REQ_TIME TIMESTAMP,
+                        REQ_HEADER TEXT,
+                        REQ_BODY TEXT
+                    )
+                    """)
+                    dbres.close()
+                    dbcreate.close()
+            finally:
+                dbcur.close()
+                
+
             c.execute("SELECT countryid, country_name, iso3, iso2, phonecode FROM sys_country")
             countries = c.fetchall()
             conn.close()
@@ -21,7 +39,7 @@ class CountriesResource(Resource):
 class CountryCDResource(Resource):
     def get(self, country_code):
         try:
-            conn = sqlite3.connect('db/address_db.sqlite3')
+            conn = sqlite3.connect(DB_DIR + "address_db.sqlite3")
             c = conn.cursor()
             c.execute("SELECT countryid, country_name, iso3, iso2, phonecode FROM sys_country \
                         WHERE iso3 = ?", (country_code,))
@@ -37,7 +55,7 @@ class CountryCDResource(Resource):
 class DivisionsResource(Resource):
     def get(self, countryid):
         try:
-            conn = sqlite3.connect('db/address_db.sqlite3')
+            conn = sqlite3.connect(DB_DIR + "address_db.sqlite3")
             c = conn.cursor()
             c.execute("SELECT  divisionid, division_cd, division_name, gso_code, los_code \
 	                    FROM  sys_division \
@@ -54,7 +72,7 @@ class DivisionsResource(Resource):
 class DivisionIDResource(Resource):
     def get(self, countryid,divisionid):
         try:
-            conn = sqlite3.connect('db/address_db.sqlite3')
+            conn = sqlite3.connect(DB_DIR + "address_db.sqlite3")
             c = conn.cursor()
             c.execute("SELECT  divisionid, division_cd, division_name, gso_code, los_code \
 	                    FROM  sys_division \
@@ -67,7 +85,7 @@ class DivisionIDResource(Resource):
 class SubdivisionsResource(Resource):
     def get(self, countryid, divisionid):
         try:
-            conn = sqlite3.connect('db/address_db.sqlite3')
+            conn = sqlite3.connect(DB_DIR + "address_db.sqlite3")
             c = conn.cursor()
             c.execute("SELECT subdivid, subdiv_cd, l2subdiv_cd, subdiv_name, los_code_sub,b.divisionid, b.countryid \
                         from sys_division_sub a, sys_division b \
--- y
+++
@@ -1,9 +0,0 @@
-version = 0.1
-[y.deploy.parameters]
-stack_name = "vietfi-address-code"
-resolve_s3 = true
-s3_prefix = "vietfi-address-code"
-region = "ap-southeast-1"
-confirm_changeset = true
-capabilities = "CAPABILITY_IAM"
-image_repositories = []
